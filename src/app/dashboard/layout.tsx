704184902b92c7e276a35cbc7221dd547e0ec4b2
shabirkhan10 <135199362+shabirkhan10@users.noreply.github.com>
2024-10-23 13:52:29 +0500

    feat: husky setup

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,13 +2,13 @@ import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-	return (
-		<SidebarProvider>
-			<AppSidebar />
-			<main>
-				<SidebarTrigger />
-				{children}
-			</main>
-		</SidebarProvider>
-	);
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
 }
